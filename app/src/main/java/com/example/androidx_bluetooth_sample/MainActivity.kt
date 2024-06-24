a481061aad7e35f58c4ebda41fb1895e9148f202
luup-tsuchiya <tsuchiya.masaki@luup.co.jp>
2024-06-24 11:38:11 +0900

    add libs.androidx.bluetooth

--- app/src/main/java/com/example/androidx_bluetooth_sample/MainActivity.kt
+++ app/src/main/java/com/example/androidx_bluetooth_sample/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidx_bluetooth_sample.ui.theme.AndroidxbluetoothsampleTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
